b1e3765904af66a6b50bb8fa14d6853538679045
Armalon <armalong@gmail.com>
2020-08-13 14:07:09 +0200

    Base components attached

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
+import Cart from './components/Cart'
+import Login from './components/Login'
+import Navigation from './components/Navigation'
+import Logo from './components/Logo'
+
 function App() {
     return (
         <div>
-            <h1>The Pizza Task</h1>
+            <div
+                className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+                <Logo></Logo>
+
+                <Navigation></Navigation>
+
+                <Login></Login>
+
+                <Cart></Cart>
+            </div>
         </div>
     );
 }
---
+++ src/components/Navigation.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const navigation = () => {
+    return <div>Navigation</div>
+}
+
+export default navigation
