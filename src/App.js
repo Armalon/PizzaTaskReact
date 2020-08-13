0d6e0d3bed64693c7e9805c05be3339c24baef4f
Armalon <armalong@gmail.com>
2020-08-13 14:03:55 +0200

    Indents fixes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div>
-      <h1>The Pizza Task</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>The Pizza Task</h1>
+        </div>
+    );
 }
 
 export default App;
