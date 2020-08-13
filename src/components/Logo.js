b19d1d31d7e1a3f659c1edc42ab49943ac2fbd4d
Armalon <armalong@gmail.com>
2020-08-13 14:58:42 +0200

    Logo base

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const logo = () => {
-    return <div>Logo</div>
+    return (<div className="my-0 mr-md-auto font-weight-normal">
+                <h1>
+                    <img src={process.env.PUBLIC_URL + '/logo.png'} style={{ marginTop: '-30px', marginBottom: '-30px'}} alt="The Pizza Cut" />
+                </h1>
+                <p className="">
+                    We deliver pieces of heaven
+                    <br />
+                    right to your apartment
+                </p>
+            </div>)
 }
 
 export default logo
